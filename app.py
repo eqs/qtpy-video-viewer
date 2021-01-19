087c8b2a7be7dce5fb6750ca5e1bd41983f12cd5
eqs <murashige.satoshi.mi1@is.naist.jp>
2021-01-19 22:51:26 +0900

    Add menu to open file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import sys
 
 from qtpy.QtWidgets import (
-    QMainWindow, QApplication, QLabel, QWidget, QVBoxLayout
+    QMainWindow, QApplication, QLabel, QWidget, QFileDialog, QAction
 )
 from qtpy.QtCore import QObject, Qt
 
@@ -16,17 +16,33 @@ class MainWindow(QMainWindow):
 
         self.main_widget = QWidget(self)
 
-        player = VideoPlayerWidget()
-        player.open_video(r'C:\Users\chikuwa\Videos\flower.mp4')
+        self.player = VideoPlayerWidget()
 
-        layout = vbox([
-            player
-        ])
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileOpenAction = QAction('Open', self)
+        fileOpenAction.triggered.connect(
+            self.on_fileOpenAction_triggered
+        )
+        fileMenu.addAction(fileOpenAction)
+
+        layout = vbox([self.player])
 
         self.main_widget.setLayout(layout)
 
         self.setCentralWidget(self.main_widget)
         self.resize(640, 480)
+
+    def on_fileOpenAction_triggered(self):
+        src_video_path, _ = QFileDialog.getOpenFileName(
+            self, 'Open video file', '.',
+            'Video files (*.avi *.mp4)'
+        )
+
+        if len(src_video_path) == 0:
+            return
+
+        self.player.open_video(src_video_path)
 
 
 def main():
