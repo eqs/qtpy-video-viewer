65d9151a93ce973fd2b5ec7023c1a33041e21da1
eqs <murashige.satoshi.mi1@is.naist.jp>
2021-01-19 20:20:49 +0900

    Add empty GUI

---
+++ app.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from qtpy.QtWidgets import (
+    QMainWindow, QApplication, QLabel, QWidget, QVBoxLayout
+)
+from qtpy.QtCore import QObject, Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, **kwargs):
+        super(MainWindow, self).__init__(**kwargs)
+
+        self.main_widget = QWidget(self)
+
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel('Hello, World!'))
+
+        self.main_widget.setLayout(layout)
+
+        self.setCentralWidget(self.main_widget)
+        self.resize(640, 480)
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    view = MainWindow()
+
+    view.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
